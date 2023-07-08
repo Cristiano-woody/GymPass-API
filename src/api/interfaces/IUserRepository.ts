42052b0f87ed4309b5e25a79878f165480941d93
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-07-08 00:07:54 -0300

    refactor: add get user by email in IUserRepository

--- src/api/interfaces/IUserRepository.ts
+++ src/api/interfaces/IUserRepository.ts
@@ -2,4 +2,5 @@ import type UserEntity from '../entities/UserEntity'
 
 export interface IUserRepository {
   create: (user: UserEntity) => Promise<void>
+  getUserByEmail: (email: string) => Promise<UserEntity | null>
 }
