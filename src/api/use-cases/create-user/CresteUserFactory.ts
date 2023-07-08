2cc567f509daa256c2896c6941966032880f4498
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-07-08 00:13:57 -0300

    feat: add createUserFactory

---
+++ src/api/use-cases/create-user/CresteUserFactory.ts
@@ -0,0 +1,10 @@
+import UserRepository from '../../repositories/UserRepository'
+import CreateUserService from './CreateUserService'
+import CreateUserController from './CreateUserController'
+
+export const cresteUserFactory = (): CreateUserController => {
+  const userRepository = new UserRepository()
+  const createUserService = new CreateUserService(userRepository)
+  const createUserController = new CreateUserController(createUserService)
+  return createUserController
+}
