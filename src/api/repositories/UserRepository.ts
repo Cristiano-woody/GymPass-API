0e8ed59637450430570f18547103ddd6de4103ec
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-07-08 00:08:50 -0300

    refactor:  add get user by email in UserRepository

--- src/api/repositories/UserRepository.ts
+++ src/api/repositories/UserRepository.ts
@@ -5,7 +5,7 @@ import UserEntity from '../entities/UserEntity'
 class UserRepository implements IUserRepository {
   async create (user: UserEntity): Promise<void> {
     const newUser = new UserEntity(user)
-    if (newUser.id !== undefined) {
+    if (newUser.id !== undefined && newUser.password_hash !== undefined) {
       await prisma.user.create({
         data: {
           name: newUser.name,
@@ -15,6 +15,15 @@ class UserRepository implements IUserRepository {
         }
       })
     }
+  }
+
+  async getUserByEmail (email: string): Promise<UserEntity | null> {
+    const user = await prisma.user.findUnique({
+      where: {
+        email
+      }
+    })
+    return user
   }
 }
 
