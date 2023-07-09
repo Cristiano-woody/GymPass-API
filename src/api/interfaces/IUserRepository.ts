8e7daff623bb8751b7597230dfbe61c8c3b804df
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-07-09 18:21:58 -0300

    refactor: new methods in IUserRepository

--- src/api/interfaces/IUserRepository.ts
+++ src/api/interfaces/IUserRepository.ts
@@ -2,5 +2,9 @@ import type UserEntity from '../entities/UserEntity'
 
 export interface IUserRepository {
   create: (user: UserEntity) => Promise<void>
+  getUser: (id: string) => Promise<UserEntity | null>
   getUserByEmail: (email: string) => Promise<UserEntity | null>
+  getAllUsers: () => Promise<UserEntity[]>
+  update: (body: { id: string, name?: string, email?: string, password_hash?: string }) => Promise<void>
+  delete: (id: string) => Promise<void>
 }
