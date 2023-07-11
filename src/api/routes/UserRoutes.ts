7d9e234e77034ae691c923a56aed60510b57e653
cristiano-woody <cristiano.nog.woody@gmail.com>
2023-07-11 16:25:06 -0300

    refactor: adding authentication route

--- src/api/routes/UserRoutes.ts
+++ src/api/routes/UserRoutes.ts
@@ -1,9 +1,11 @@
 import * as express from 'express'
 import { type Request, type Response } from 'express'
 import { cresteUserFactory } from '../use-cases/create-user/CresteUserFactory'
+import { authenticateFactory } from '../use-cases/authenticate/AuthenticateFactory'
 
 const userRoutes = express.Router()
 
-userRoutes.post('/user', (req: Request, res: Response) => { void cresteUserFactory().handle(req, res) })
+userRoutes.post('/register', (req: Request, res: Response) => { void cresteUserFactory().handle(req, res) })
+userRoutes.post('/authenticate', (req: Request, res: Response) => { void authenticateFactory().handle(req, res) })
 
 export default userRoutes
