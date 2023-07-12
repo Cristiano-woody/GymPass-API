ddca9332dd5942116db5c6ce74de42ebfe098442
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-07-12 19:27:50 -0300

    refactor: creating GetUserProfile route

--- src/api/routes/UserRoutes.ts
+++ src/api/routes/UserRoutes.ts
@@ -1,11 +1,13 @@
 import * as express from 'express'
 import { type Request, type Response } from 'express'
-import { cresteUserFactory } from '../use-cases/create-user/CresteUserFactory'
-import { authenticateFactory } from '../use-cases/authenticate/AuthenticateFactory'
+import { createUserFactory } from '../use-cases/create-user/createUserFactory'
+import { authenticateFactory } from '../use-cases/authenticate/authenticateFactory'
+import { getUserprofileFactory } from '../use-cases/get-user-profile/getUserProfileFactory'
 
 const userRoutes = express.Router()
 
-userRoutes.post('/register', (req: Request, res: Response) => { void cresteUserFactory().handle(req, res) })
+userRoutes.post('/register', (req: Request, res: Response) => { void createUserFactory().handle(req, res) })
 userRoutes.post('/authenticate', (req: Request, res: Response) => { void authenticateFactory().handle(req, res) })
+userRoutes.get('/profile/:id', (req: Request, res: Response) => { void getUserprofileFactory().handle(req, res) })
 
 export default userRoutes
