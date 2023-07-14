faa9d8176cc6eb7038b3ffb3900fd183e2decc48
cristiano-woody <cristiano.nog.woody@gmail.com>
2023-07-14 16:20:11 -0300

    refactor: adding method in ICheckInRepository

--- src/api/interfaces/ICheckinRepository.ts
+++ src/api/interfaces/ICheckinRepository.ts
@@ -2,4 +2,5 @@ import type CheckInEntity from '../entities/CkeckinEntity'
 
 export interface ICheckinRepository {
   create: (data: CheckInEntity) => Promise<CheckInEntity | undefined>
+  fyndByUserInOnDate: (data: { date: Date, userId: string }) => Promise<CheckInEntity | undefined>
 }
