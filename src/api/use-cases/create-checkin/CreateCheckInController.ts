2f0be439b2a3974676049884e95f201d6ce426b4
cristiano-woody <cristiano.nog.woody@gmail.com>
2023-07-14 16:05:45 -0300

    feat: creating a CreatecheckIncontroller

---
+++ src/api/use-cases/create-checkin/CreateCheckInController.ts
@@ -0,0 +1,33 @@
+import { type Request, type Response } from 'express'
+import { InvalidCredentialsError } from '../../errors/InvalidCredentialsError'
+import { type ICreateCheckInService } from '../../interfaces/ICreateCheckInService'
+import { ResourceNotFoundError } from '../../errors/ResourceNotFoundError'
+import { TwoCheckInsAreNotAlowed } from '../../errors/TwoCheckInsAreNotAlowed'
+
+class CreateCheckInController {
+  constructor (private readonly CreateUserService: ICreateCheckInService) {}
+  async handle (req: Request, res: Response): Promise<Response> {
+    try {
+      await this.CreateUserService.execute(req.body)
+      return res.status(201).send('CheckIn created successfully')
+    } catch (error) {
+      if (error instanceof InvalidCredentialsError) {
+        return res.status(400).send({ message: error.message })
+      }
+      if (error instanceof ResourceNotFoundError) {
+        return res.status(400).send({ message: error.message })
+      }
+      if (error instanceof TwoCheckInsAreNotAlowed) {
+        return res.status(400).send({ message: error.message })
+      }
+
+      if (error instanceof Error) {
+        return res.status(500).send({ message: error.message })
+      }
+
+      return res.status(500).send({ message: 'internal server error' })
+    }
+  }
+}
+
+export default CreateCheckInController
