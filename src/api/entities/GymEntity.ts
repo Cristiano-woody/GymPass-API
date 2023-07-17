3eb7bb84cca14f0c5ecfb373f87599393c8e2f45
cristiano-woody <cristiano.nog.woody@gmail.com>
2023-07-17 16:43:25 -0300

    feat: creating Gym Entity

---
+++ src/api/entities/GymEntity.ts
@@ -0,0 +1,23 @@
+import { v4 as uuidv4 } from 'uuid'
+
+class GymEntity {
+  public id?: string
+  public title: string
+  public description?: string | null
+  public phone?: string | null
+  public latitude: number
+  public longitude: number
+
+  constructor (data: GymEntity) {
+    this.title = data.title
+    this.description = data.description
+    this.id = data.id
+    this.latitude = data.latitude
+    this.longitude = data.longitude
+    if (data.id === undefined || data.id === null) {
+      this.id = uuidv4()
+    }
+  }
+}
+
+export default GymEntity
