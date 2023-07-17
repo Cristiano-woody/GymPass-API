0442032c4a0633d5524dbd1b2a53a39c7d0f8393
cristiano-woody <cristiano.nog.woody@gmail.com>
2023-07-17 16:49:04 -0300

    test: adding new test in CreateCheckInService

--- src/api/use-cases/create-checkin/CreateCheckInService.spec.ts
+++ src/api/use-cases/create-checkin/CreateCheckInService.spec.ts
@@ -5,44 +5,125 @@ import { InvalidCredentialsError } from '../../errors/InvalidCredentialsError'
 import InMemoryUserRepository from '../../repositories/in-memory/InMemoryUserRepository'
 import { ResourceNotFoundError } from '../../errors/ResourceNotFoundError'
 import { TwoCheckInsAreNotAlowed } from '../../errors/TwoCheckInsAreNotAlowed'
+import InMemoryGymRepository from '../../repositories/in-memory/InMemoryGymRepository'
+import { MaxDistanceError } from '../../errors/MaxDistanceError'
 
 let checkInRepository: InMemoryCheckInRepository
+let userRepository: InMemoryUserRepository
+let gymRepository: InMemoryGymRepository
 let createCheckInService: CreateCheckInService
-let UserRepository: InMemoryUserRepository
 
 describe('Create a checkIn ', () => {
   beforeEach(async () => {
     checkInRepository = new InMemoryCheckInRepository()
-    UserRepository = new InMemoryUserRepository()
-    createCheckInService = new CreateCheckInService(checkInRepository, UserRepository)
-    const user = { email: 'johndoe@example.com', name: 'john doe', password_hash: '1234', id: 'user1234' }
-    void UserRepository.create(user)
+    userRepository = new InMemoryUserRepository()
+    gymRepository = new InMemoryGymRepository()
+    createCheckInService = new CreateCheckInService(checkInRepository, userRepository, gymRepository)
+
+    void gymRepository.create({ id: '123', title: 'tsAcademy', latitude: -8.0481364, longitude: -34.8791966 })
+    void userRepository.create({ email: 'johndoe@example.com', name: 'john doe', password_hash: '1234', id: 'user1234' })
     vi.useFakeTimers()
   })
+
   afterEach(() => {
     vi.useRealTimers()
   })
+
   it('shold be able to create a checkIn', async () => {
-    await expect(createCheckInService.execute({ gymId: 'asd12e-123wsd-12412', userId: 'user1234' })).resolves.not.toThrow()
+    await expect(createCheckInService.execute(
+      {
+        gymId: '123',
+        userId: 'user1234',
+        userCoordinates: { latitude: -8.0481364, longitude: -34.8791966 }
+      }
+    )).resolves.not.toThrow()
   })
+
   it('shold not be able to create a checkIn with empty userId field', async () => {
-    await expect(createCheckInService.execute({ gymId: 'asd12e-123wsd-12412', userId: '' })).rejects.toThrow(new InvalidCredentialsError())
+    await expect(createCheckInService.execute(
+      {
+        gymId:
+        'asd12e-123wsd-12412',
+        userId: '',
+        userCoordinates: { latitude: -8.0481364, longitude: -34.8791966 }
+      }
+    )).rejects.toThrow(new InvalidCredentialsError())
   })
+
   it('shold not be able to create a checkIn with empty gymId field', async () => {
-    await expect(createCheckInService.execute({ gymId: '', userId: 'asdq13-sdfw12-sfsdf3' })).rejects.toThrow(new InvalidCredentialsError())
+    await expect(createCheckInService.execute(
+      {
+        gymId: '',
+        userId: 'asdq13-sdfw12-sfsdf3',
+        userCoordinates: { latitude: -8.0481364, longitude: -34.8791966 }
+      }
+    )).rejects.toThrow(new InvalidCredentialsError())
   })
+
   it('should not be able to create a CheckIn with unregistered user', async () => {
-    await expect(createCheckInService.execute({ gymId: 'asd12e-123wsd-12412', userId: 'user Does not exist' })).rejects.toThrow(new ResourceNotFoundError())
+    await expect(createCheckInService.execute(
+      {
+        gymId: 'asd12e-123wsd-12412',
+        userId: 'user Does not exist',
+        userCoordinates: { latitude: -8.0481364, longitude: -34.8791966 }
+      }
+    )).rejects.toThrow(new ResourceNotFoundError())
   })
+
   it('shold not be able to check in twice inthe same day', async () => {
     vi.setSystemTime(new Date(2023, 0, 20, 8, 0, 0))
-    await createCheckInService.execute({ gymId: 'asd12e-123wsd-12412', userId: 'user1234' })
-    await expect(createCheckInService.execute({ gymId: 'asd12e-123wsd-12412', userId: 'user1234' })).rejects.toThrow(new TwoCheckInsAreNotAlowed())
+    await createCheckInService.execute(
+      {
+        gymId: '123',
+        userId: 'user1234',
+        userCoordinates: { latitude: -8.0481364, longitude: -34.8791966 }
+      })
+
+    await expect(createCheckInService.execute(
+      {
+        gymId: '123',
+        userId: 'user1234',
+        userCoordinates: { latitude: -8.0481364, longitude: -34.8791966 }
+      }
+    )).rejects.toThrow(new TwoCheckInsAreNotAlowed())
   })
+
   it('shold be able to check in twice but in different days', async () => {
     vi.setSystemTime(new Date(2023, 0, 20, 8, 0, 0))
-    await createCheckInService.execute({ gymId: 'asd12e-123wsd-12412', userId: 'user1234' })
+    await createCheckInService.execute(
+      {
+        gymId: '123',
+        userId: 'user1234',
+        userCoordinates: { latitude: -8.0481364, longitude: -34.8791966 }
+      })
+
     vi.setSystemTime(new Date(2023, 0, 21, 8, 0, 0))
-    await expect(createCheckInService.execute({ gymId: 'asd12e-123wsd-12412', userId: 'user1234' })).resolves.not.toThrow()
+    await expect(createCheckInService.execute(
+      {
+        gymId: '123',
+        userId: 'user1234',
+        userCoordinates: { latitude: -8.0481364, longitude: -34.8791966 }
+      }
+    )).resolves.not.toThrow()
+  })
+
+  it('shold be able to create check in with a distance of less than 100 meters', async () => {
+    await expect(createCheckInService.execute(
+      {
+        gymId: '123',
+        userId: 'user1234',
+        userCoordinates: { latitude: -8.048729, longitude: -34.879487 }
+      }
+    )).resolves.not.toThrow()
+  })
+
+  it('shold not be able to create check in with a distance greater than 100 meters', async () => {
+    await expect(createCheckInService.execute(
+      {
+        gymId: '123',
+        userId: 'user1234',
+        userCoordinates: { latitude: -8.0490052, longitude: -34.8884457 }
+      }
+    )).rejects.toThrow(new MaxDistanceError())
   })
 })
