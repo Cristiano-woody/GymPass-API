2c0afc6c2c4d5f286359fdd064a9e36a0fd58ea5
cristiano-woody <cristiano.nog.woody@gmail.com>
2023-07-17 16:50:27 -0300

    refactor: addind new conditionals CreateCheckInSer

--- src/api/use-cases/create-checkin/CreateCheckinService.ts
+++ src/api/use-cases/create-checkin/CreateCheckinService.ts
@@ -1,15 +1,18 @@
-import type CheckInEntity from '../../entities/CkeckinEntity'
 import { type ICheckinRepository } from '../../interfaces/ICheckinRepository'
 import { type ICreateCheckInService } from '../../interfaces/ICreateCheckInService'
 import { InvalidCredentialsError } from '../../errors/InvalidCredentialsError'
 import { type IUserRepository } from '../../interfaces/IUserRepository'
 import { ResourceNotFoundError } from '../../errors/ResourceNotFoundError'
 import { TwoCheckInsAreNotAlowed } from '../../errors/TwoCheckInsAreNotAlowed'
+import { type IGymRepository } from '../../interfaces/IGymRepository'
+import { getDistanceBetweenCoordinates } from '../../helpers/getDistanceBetweenCoordinates'
+import { MaxDistanceError } from '../../errors/MaxDistanceError'
 
 class CreateCheckInService implements ICreateCheckInService {
-  constructor (private readonly CheckInRepository: ICheckinRepository, private readonly UserRepository: IUserRepository) {}
+  constructor (private readonly CheckInRepository: ICheckinRepository, private readonly UserRepository: IUserRepository, private readonly GymRepository: IGymRepository) {}
 
-  async execute (data: CheckInEntity): Promise<void> {
+  async execute (
+    data: { userId: string, gymId: string, userCoordinates: { latitude: number, longitude: number } }): Promise<void> {
     if (data.gymId === undefined || data.gymId === '') {
       throw new InvalidCredentialsError()
     }
@@ -21,6 +24,20 @@ class CreateCheckInService implements ICreateCheckInService {
 
     if (!userAlreadyExists) {
       throw new ResourceNotFoundError()
+    }
+
+    const gym = await this.GymRepository.getGym(data.gymId)
+
+    if (gym === undefined) {
+      throw new ResourceNotFoundError()
+    }
+
+    const distance = getDistanceBetweenCoordinates({ latitude: gym.latitude, longitude: gym.longitude }, { latitude: data.userCoordinates.latitude, longitude: data.userCoordinates.longitude })
+
+    const maximumDistanceAllowedInKM = 0.1
+
+    if (distance > maximumDistanceAllowedInKM) {
+      throw new MaxDistanceError()
     }
 
     const checkInOnSameDay = await this.CheckInRepository.fyndByUserInOnDate({ userId: data.userId, date: new Date() })
