a6a6c7caa9ee336a1389d63f63671aed6a179f2a
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-07-21 17:12:00 -0300

    refactor: creating route checkin

--- src/api/routes/UserRoutes.ts
+++ src/api/routes/UserRoutes.ts
@@ -3,11 +3,13 @@ import { type Request, type Response } from 'express'
 import { createUserFactory } from '../use-cases/create-user/createUserFactory'
 import { authenticateFactory } from '../use-cases/authenticate/authenticateFactory'
 import { getUserprofileFactory } from '../use-cases/get-user-profile/getUserProfileFactory'
+import { CreateCheckInFactory } from '../use-cases/create-checkin/CreateCheckInFactory'
 
 const userRoutes = express.Router()
 
 userRoutes.post('/register', (req: Request, res: Response) => { void createUserFactory().handle(req, res) })
 userRoutes.post('/authenticate', (req: Request, res: Response) => { void authenticateFactory().handle(req, res) })
 userRoutes.get('/profile/:id', (req: Request, res: Response) => { void getUserprofileFactory().handle(req, res) })
+userRoutes.post('/checkin', (req: Request, res: Response) => { void CreateCheckInFactory().handle(req, res) })
 
 export default userRoutes
