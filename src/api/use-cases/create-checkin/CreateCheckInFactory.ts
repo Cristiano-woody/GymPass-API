643b1dc160ce1e75141d3ca5415c90096a77d1f9
cristiano <cristiano.nog.woody@gmail.com>
2023-09-04 18:01:09 -0300

    refactor: modified createCheckInFactory

--- src/api/use-cases/create-checkin/CreateCheckInFactory.ts
+++ src/api/use-cases/create-checkin/CreateCheckInFactory.ts
@@ -2,13 +2,11 @@ import UserRepository from '../../repositories/UserRepository'
 import GymRepository from '../../repositories/GymRepository'
 import CheckInRepository from '../../repositories/CheckInRepository'
 import CreateCheckInService from './CreateCheckinService'
-import CreateCheckInController from './CreateCheckInController'
 
-export const CreateCheckInFactory = (): CreateCheckInController => {
+export const CreateCheckInFactory = (): CreateCheckInService => {
   const userRepository = new UserRepository()
   const gymRepository = new GymRepository()
   const checkInRepository = new CheckInRepository()
   const checkInService = new CreateCheckInService(checkInRepository, userRepository, gymRepository)
-  const checkInController = new CreateCheckInController(checkInService)
-  return checkInController
+  return checkInService
 }
