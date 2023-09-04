30038171ec9e721f87beca7f316c8384b9fbf82a
cristiano <cristiano.nog.woody@gmail.com>
2023-09-04 18:02:07 -0300

    refactor: modified AuthenticateFactory

--- src/api/use-cases/authenticate/authenticateFactory.ts
+++ src/api/use-cases/authenticate/authenticateFactory.ts
@@ -1,10 +1,8 @@
 import UserRepository from '../../repositories/UserRepository'
 import AuthenticateService from './AuthenticateService'
-import AuthenticateController from './AuthenticateController'
 
-export const authenticateFactory = (): AuthenticateController => {
+export const authenticateFactory = (): AuthenticateService => {
   const userRepository = new UserRepository()
   const authenticateService = new AuthenticateService(userRepository)
-  const authenticateController = new AuthenticateController(authenticateService)
-  return authenticateController
+  return authenticateService
 }
