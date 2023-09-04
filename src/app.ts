41b1528026ae942b4b412aa723dabc6655aa2da8
cristiano <cristiano.nog.woody@gmail.com>
2023-09-04 18:04:07 -0300

    Refactor: Using Routes in App

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from 'express'
 import cors from 'cors'
-import userRoutes from './api/routes/UserRoutes'
+import userRoutes from './api/http/routes/userRoutes'
+import gymRoutes from './api/http/routes/gymRoutes'
+import chckInRoutes from './api/http/routes/checkInRoutes'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 app.use(userRoutes)
+app.use(gymRoutes)
+app.use(chckInRoutes)
 
 export default app
