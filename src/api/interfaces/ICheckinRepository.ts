0a229de0e40a9041e4a3cb9d8ba1288b6861a86f
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-09-05 15:24:20 -0300

    refactor: adding method in ICheckInRepository

--- src/api/interfaces/ICheckinRepository.ts
+++ src/api/interfaces/ICheckinRepository.ts
@@ -4,4 +4,5 @@ export interface ICheckinRepository {
   create: (data: CheckInEntity) => Promise<CheckInEntity | undefined>
   fyndByUserInOnDate: (data: { date: Date, userId: string }) => Promise<CheckInEntity | undefined>
   findManyCheckInsByUserId: (id: string, page: number) => Promise<CheckInEntity[]>
+  countByUserId: (Id: string) => Promise<number>
 }
