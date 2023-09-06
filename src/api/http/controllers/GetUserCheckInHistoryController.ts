6b27e2d7e86bbcb53c99d200c528b5a4789f352e
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-09-06 15:15:39 -0300

    refactor: adding fields check

--- src/api/http/controllers/GetUserCheckInHistoryController.ts
+++ src/api/http/controllers/GetUserCheckInHistoryController.ts
@@ -1,6 +1,6 @@
 import { type Request, type Response } from 'express'
 import { type IGetUserCheckInHistoryService } from '../../interfaces/IGetUserCheckInHistoryService'
-import { getUserCheckInHistoryFactory } from '../../use-cases/get-user-check-in-history/GetUserCheckInHistoryFactory'
+import { getUserCheckInHistoryFactory } from '../../use-cases/get-user-check-in-history/getUserCheckInHistoryFactory'
 
 class GetUserCheckInHistoryController {
   private readonly getUserCheckInHistoryService: IGetUserCheckInHistoryService
@@ -10,8 +10,15 @@ class GetUserCheckInHistoryController {
   }
 
   async handle (req: Request, res: Response): Promise<Response> {
+    const { id, page } = req.params
+    if (id === '' || id === undefined || id === null) {
+      return res.status(400).json({ error: 'invalid credentials error.' })
+    }
+    if (page === '' || page === undefined || page === null) {
+      return res.status(400).json({ error: 'invalid credentials error.' })
+    }
     try {
-      const User = await this.getUserCheckInHistoryService.execute(req.params.id, parseInt(req.params.page))
+      const User = await this.getUserCheckInHistoryService.execute(id, parseInt(page))
       return res.status(200).json(User)
     } catch (error) {
       if (error instanceof Error) {
